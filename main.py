fa4f1abd2a1110db188cd9210e113354926ab600
YuliaCheprasova <cheprasovaja@yandex.ru>
2023-02-21 11:21:09 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+class person():
+    def wish(self):
+        pass
+    def place(self, who):
+        pass
+    def time(self, who, where):
+        pass
+class time_machine():
+    def request(self, who, where, when):
+        pass
+    def result(self):
+        pass
